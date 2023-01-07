94340c616d0ab7c3710ba0cc66594f8cdd6002f8
kursad68 <aykinlar6806@gmail.com>
2023-01-07 05:05:52 +0300

    Fix

--- Assets/Scripts/Aı/AıShip.cs
+++ Assets/Scripts/Aı/AıShip.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AıShip : MonoBehaviour
+public class AıShip : ShipBase
 {
+    public StateAı currentState;
+    public StateAı fireState=new FireState(), fightState=new FightState(), followState=new FollowState();
+
+    
+    private void Update()
+    {
+        currentState.UpdateState(this);
+    }
+    private void SwitchState(StateAı switchAı)
+    {
+        currentState = switchAı;
+    }
     public void MoveRight()
     {
         
@@ -19,5 +31,51 @@ public class AıShip : MonoBehaviour
     public void MoveDown()
     {
 
+    }
+}
+public abstract class StateAı
+{
+    public abstract void EnterState(AıShip aıShip);
+
+    public abstract void UpdateState(AıShip aıShip);
+}
+public class FireState : StateAı
+{
+    public override void EnterState(AıShip aıShip)
+    {
+        
+    }
+    public override void UpdateState(AıShip aıShip)
+    {
+        
+    }
+}
+public class FightState : StateAı
+{
+    public override void EnterState(AıShip aıShip)
+    {
+        
+    }
+    public override void UpdateState(AıShip aıShip)
+    {
+        
+    }
+}
+public class FollowState : StateAı
+{
+    Collider[] collider;
+    int x;
+
+    public override void EnterState(AıShip aıShip)
+    {
+        
+    }
+    public override void UpdateState(AıShip aiShip)
+    {
+         x= Physics.OverlapSphereNonAlloc(aiShip.transform.position, 3f, collider, 1);
+        for (int i = 0; i <x; i++)
+        {
+
+        }
     }
 }
